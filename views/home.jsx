7620fe6b01a21dd62eb6aed2fb590457f919f371
Keifer Rasner <rasnerkeifer@gmail.com>
2022-05-15 12:30:43 -0500

    Adding styles to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -3,11 +3,14 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
 }  
 
