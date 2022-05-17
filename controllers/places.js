e634a3a0798fd74f8790ae74f3609f148e38910e
Keifer Rasner <rasnerkeifer@gmail.com>
2022-05-17 11:35:44 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,33 +1,27 @@
 const router = require('express').Router();
+const places = require('../models/places')
 
 router.get('/', (req,res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/light-city-restaurant.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/rest-2.jpg'
-      },
-      {
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/light-city-restaurant.jpg'
-      },
-    ]      
-    res.render('places/index', {places})
+    res.render('places/index', { places })
 })
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
 
 module.exports = router;
